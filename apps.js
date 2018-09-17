33f11677cdbe50b8b557db57ce467f62a12dc5d4
Jessica Duell <duellrdh@gmail.com>
2018-09-16 20:49:25 -0400

    updated

---
+++ apps.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+   let upper = $(document.getElementById("keyboard-upper-container"));
+   let lower = $(document.getElementById("keyboard-lower-container"));
+
+//hides the upper case keyboard when the page loads 
+   upper.hide();
+
+//when the shift key is held down the lower case keyboard is hidden and the upper case keyboard is shown
+
+
+})
